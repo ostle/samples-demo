9fcbf32c4418849831abb701e5c7cf20563502d7
ostle <leoostrower@gmail.com>
2023-08-22 13:24:27 -0300

    nav fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,10 +2,12 @@
 
 import { Search, User, ShoppingBag } from "lucide-react";
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 const Navbar = () => {
 	const [isMounted, setIsMounted] = useState(false);
 	const [isScrolled, setIsScrolled] = useState(false);
+	const [isShopHovered, setIsShopHovered] = useState(false);
 
 	const handleEnter = () => {
 		setIsMounted(true);
@@ -45,8 +47,12 @@ const Navbar = () => {
 					<div className='flex items-center space-x-4 text-sm'>
 						<div
 							className={`text-sm leading-tight ${
-								isMounted || isScrolled ? "text-black" : ""
+								((isMounted || isScrolled) && !isShopHovered) ||
+								isShopHovered // Agregar esta condición
+									? "text-black"
+									: ""
 							}`}
+							onMouseEnter={() => setIsShopHovered(true)}
 						>
 							SHOP
 						</div>
@@ -86,37 +92,42 @@ const Navbar = () => {
 						<ShoppingBag size={20} />
 					</div>
 				</div>
-				{isMounted && (
+				{isShopHovered && (
 					<div
-						className={`box-cover h-1/3 ${
-							isScrolled ? "bg-white" : ""
-						}`}
+						onMouseEnter={() => setIsShopHovered(true)}
+						onMouseLeave={() => setIsShopHovered(false)}
 					>
-						<div className='flex flex-col md:flex-row justify-between px-4 py-2 mb-0'>
-							<div className='w-full md:w-3/4'>
-								<div className='ml-8 mt-4'>
-									<div className='font-bold text-md mb-4'>
-										Categories
+						<div
+							className={`box-cover h-1/3 ${
+								isScrolled ? "bg-white" : ""
+							}`}
+						>
+							<div className='flex flex-col md:flex-row justify-between px-4 py-2 mb-0'>
+								<div className='w-full md:w-3/4'>
+									<div className='ml-8 mt-4'>
+										<div className='font-bold text-md mb-4'>
+											Categories
+										</div>
+										<ul className='list-none text-sm'>
+											<li>Hoodies & Crewnecks</li>
+											<li>Jackets</li>
+											<li>Pants</li>
+											<li>Tees</li>
+											<li>Accessories</li>
+											<li>Shirts</li>
+											<li>Shorts</li>
+											<li>Sets</li>
+										</ul>
 									</div>
-									<ul className='list-none text-sm'>
-										<li>Hoodies & Crewnecks</li>
-										<li>Jackets</li>
-										<li>Pants</li>
-										<li>Tees</li>
-										<li>Accessories</li>
-										<li>Shirts</li>
-										<li>Shorts</li>
-										<li>Sets</li>
-									</ul>
 								</div>
-							</div>
-							<div className='w-full md:w-1/4 mt-4 md:mt-0'>
-								<div className='box-cover'>
-									<img
-										src='https://acdn.mitiendanube.com/stores/001/759/686/products/0n7a91281-a0a96fdf1f78af4c4616881494937969-1024-1024.webp'
-										alt='Imagen'
-										className='object-cover w-full h-full'
-									/>
+								<div className='w-full md:w-1/4 mt-4 md:mt-0'>
+									<div className='box-cover'>
+										<img
+											src='https://acdn.mitiendanube.com/stores/001/759/686/products/two083411-8b8c0a707b739785d016908970583013-1024-1024.webp'
+											alt='Imagen'
+											className='object-cover w-full h-full'
+										/>
+									</div>
 								</div>
 							</div>
 						</div>
